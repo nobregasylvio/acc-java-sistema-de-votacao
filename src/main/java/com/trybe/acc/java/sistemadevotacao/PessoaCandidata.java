6a25884bde491c60cc5c2c201ca39cca7b93e634
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-05-21 18:46:11 -0300

    feat: implementando a classe pessoaCandidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,36 @@
 package com.trybe.acc.java.sistemadevotacao;
 
 public class PessoaCandidata {
+	private String name;
+	private int number;
+	private int numberOfVotes;
 
+	public PessoaCandidata(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void oneMoreVote() {
+		this.numberOfVotes += 1;
+	}
+
+	public int getNumberOfVotes() {
+		return numberOfVotes;
+	}
 }
