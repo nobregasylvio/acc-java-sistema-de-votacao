759dc7742c17406c00b3a77e3e79994df8ca1691
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-05-21 20:33:35 -0300

    feat: implementado menu de repetição para registrar candidato

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -1,9 +1,33 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
-
   public static void main(String[] args) {
+    Boolean registerCandidate = true;
+    Scanner sc = new Scanner(System.in);
 
+    while (registerCandidate) {
+      System.out.println("Cadastrar pessoa candidata?\n"
+          + "1 - Sim\n"
+          + "2 - Não\n"
+          + "Entre com o número correspondente à opção desejada:");
+
+      short responseRegisterCandidate = sc.nextShort();
+
+      if (responseRegisterCandidate == 1) {
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String name = sc.next();
+
+        System.out.println("Entre com o número da pessoa candidata:");
+        int number = sc.nextInt();
+
+      } else {
+        registerCandidate = false;
+      }
+    }
+
+    sc.close();
   }
 
 }
