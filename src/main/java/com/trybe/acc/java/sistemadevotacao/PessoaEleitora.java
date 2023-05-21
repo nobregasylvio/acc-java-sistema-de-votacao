996ebb109a31aa13e4dd4a165ab3143770cd58c0
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-05-21 20:09:02 -0300

    feat: implementando a classe PessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,14 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
+	private String cpf;
 
+	public PessoaEleitora(String name, String cpf) {
+		super.name = name;
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
 }
