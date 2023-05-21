d012e5a16b5e5173aebeb5c402f51f9d252f9532
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-05-21 20:39:30 -0300

    feat: implementado menu de repetição para registrar eleitor

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Principal {
   public static void main(String[] args) {
-    Boolean registerCandidate = true;
+    Boolean isRegisterCandidate = true;
+    Boolean isRegisterVoter = true;
     Scanner sc = new Scanner(System.in);
 
-    while (registerCandidate) {
+    while (isRegisterCandidate) {
       System.out.println("Cadastrar pessoa candidata?\n"
           + "1 - Sim\n"
           + "2 - Não\n"
           + "Entre com o número correspondente à opção desejada:");
 
-      short responseRegisterCandidate = sc.nextShort();
+      short option = sc.nextShort();
 
-      if (responseRegisterCandidate == 1) {
+      if (option == 1) {
         System.out.println("Entre com o nome da pessoa candidata:");
         String name = sc.next();
 
@@ -23,8 +24,28 @@ public class Principal {
         int number = sc.nextInt();
 
       } else {
-        registerCandidate = false;
+        isRegisterCandidate = false;
       }
+    }
+
+    while (isRegisterVoter) {
+      System.out.println("Cadastrar pessoa eleitora?\n"
+          + "1 - Sim\n"
+          + "2 - Não\n"
+          + "Entre com o número correspondente à opção desejada:");
+
+      short option = sc.nextShort();
+
+      if (option == 1) {
+        System.out.println("Entre com o nome da pessoa eleitora:");
+        String name = sc.next();
+
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpf = sc.next();
+      } else {
+        isRegisterVoter = false;
+      }
+
     }
 
     sc.close();
