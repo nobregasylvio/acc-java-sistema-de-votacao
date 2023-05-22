7595865c72422a5d30570f77be0b6ffb90faf1eb
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2023-05-22 02:35:31 -0300

    feat: implementando o metodo cadastrarPessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class GerenciamentoVotacao {
 	private ArrayList<PessoaCandidata> pessoasCandidatas;
+	private ArrayList<PessoaEleitora> pessoasEleitoras;
 
 	public GerenciamentoVotacao() {
 		this.pessoasCandidatas = new ArrayList<>();
+		this.pessoasEleitoras = new ArrayList<>();
 	}
 
 	public void cadastrarPessoaCandidata(String nome, int numero) {
@@ -14,7 +16,7 @@ public class GerenciamentoVotacao {
 			if (pessoa.getNome().equals(nome) || pessoa.getNumero() == numero) {
 				System.out.println("Número pessoa candidata já utilizado!");
 				return;
-			}	
+			}
 		}
 
 		PessoaCandidata candidato = new PessoaCandidata(nome, numero);
@@ -22,7 +24,15 @@ public class GerenciamentoVotacao {
 	}
 
 	public void cadastrarPessoaEleitora(String nome, String cpf) {
+		for (PessoaEleitora pessoa : pessoasEleitoras) {
+			if (pessoa.getNome().equals(nome) || pessoa.getCpf().equals(cpf)) {
+				System.out.println("Pessoa eleitora já cadastrada!");
+				return;
+			}
+		}
 
+		PessoaEleitora candidato = new PessoaEleitora(nome, cpf);
+		pessoasEleitoras.add(candidato);
 	}
 
 	public void votar(String cpf, int numero) {
